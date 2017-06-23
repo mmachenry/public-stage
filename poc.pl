e54e99e0f00bdd04df27a3511d483f6b3910ddfd
Mike MacHenry <mike.machenry@gmail.com>
2017-06-22 20:07:31 -0400

    volume

--- poc.pl
+++ poc.pl
@@ -42,6 +42,7 @@ if ($is_playing) {
     print "Currently playing. ";
     if ($should_play) {
         print "Nothing to do.\n";
+        system("amixer set Master 50%");
     } else {
         print "Should stop playback.\n";
     }
