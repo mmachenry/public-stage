291603724e3dd5f141f98cc2ef641ba44ffd49fa
Mike MacHenry <mike.machenry@gmail.com>
2017-08-13 22:22:00 -0400

    notes from how to install

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+sudo touch /boot/ssh
+sudo apt-get install git mpd mpc python-mpd
 sudo cp interfaces /etc/network/
 sudo cp wpa_supplicant.conf /etc/wpa_supplicant/
+git clone https://github.com/mmachenry/public-stage.git
+sudo vi /etc/mpd.conf # change bind_to_address from "localhost" to "::"
+crontab -e # add crontab.txt lines
