5da0934eddd9ac6e5dbc97c33d941468c2946a0f
Mike MacHenry <mike.machenry@gmail.com>
2017-08-13 22:56:52 -0400

    updates to process

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ sudo cp wpa_supplicant.conf /etc/wpa_supplicant/
 git clone https://github.com/mmachenry/public-stage.git
 sudo vi /etc/mpd.conf # change bind_to_address from "localhost" to "::"
 crontab -e # add crontab.txt lines
+sudo raspi-config # set timezone to eastern or whereever we set the schedule for
+sudo vi /etc/rc.local # add /etc/init.d/cron start on last line
